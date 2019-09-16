4382ff86acc1ace63d43fbe36fc380e63b2f2b09
Russ Tedrake <russt@mit.edu>
2019-09-15 21:52:43 -0400

    rgbd sensor looking at a mustard bottle

--- src/first_point_cloud.py
+++ src/first_point_cloud.py
@@ -1,30 +1,60 @@
-from pydrake.examples.manipulation_station import ManipulationStation
+import matplotlib.pyplot as plt
+import numpy as np
+
+from pydrake.common import FindResourceOrThrow
+from pydrake.geometry.render import DepthCameraProperties, MakeRenderEngineVtk, RenderEngineVtkParams
+from pydrake.math import RigidTransform, RollPitchYaw
+from pydrake.multibody.parsing import Parser
+from pydrake.multibody.plant import AddMultibodyPlantSceneGraph
 from pydrake.systems.framework import DiagramBuilder
 from pydrake.systems.meshcat_visualizer import (
   MeshcatVisualizer, MeshcatPointCloudVisualizer )
+from pydrake.systems.sensors import RgbdSensor
 
 builder = DiagramBuilder()
 
-# Set up the manipulation station.
-station = builder.AddSystem(ManipulationStation())
-station.SetupManipulationClassStation()
-station.Finalize()
+# Create the physics engine + scene graph.
+plant, scene_graph = AddMultibodyPlantSceneGraph(builder)
+# Add a single object into it.
+Parser(plant, scene_graph).AddModelFromFile(FindResourceOrThrow(
+  "drake/manipulation/models/ycb/sdf/006_mustard_bottle.sdf"))
+# Add a rendering engine
+renderer = "my_renderer"
+scene_graph.AddRenderer(renderer, MakeRenderEngineVtk(RenderEngineVtkParams()))
+plant.Finalize()
 
-# Visualize the station geometry in meshcat.
-meshcat = builder.AddSystem(MeshcatVisualizer(station.get_scene_graph()))
-builder.Connect(station.GetOutputPort("pose_bundle"),
-                        meshcat.get_input_port(0))
+# Add a visualizer just to help us see the object.
+use_meshcat = False
+if use_meshcat:
+  meshcat = builder.AddSystem(MeshcatVisualizer(scene_graph))
+  builder.Connect(scene_graph.get_pose_bundle_output_port(),
+      meshcat.get_input_port(0))
 
-# Convert the depth camera output into a point cloud
-builder.AddSystem(DepthImageToPointCloud())
-
-# TODO(russt) set camera pose.
-
-meshlab_point_cloud = builder.AddSystem(MeshcatPointCloudVisualizer(meshcat))
-builder.Connect()
+# Add a camera to the environment.
+pose = RigidTransform(RollPitchYaw(-0.2, 0.2, 0), [-.1, -0.1, -.5])  
+properties = DepthCameraProperties(width=640, height=480, fov_y=np.pi/4.0, renderer_name=renderer, z_near=0.1, z_far=10.0)
+camera = builder.AddSystem(RgbdSensor(parent_id=scene_graph.world_frame_id(),
+  X_PB=pose, properties=properties, show_window=False))
+builder.Connect(scene_graph.get_query_output_port(),   
+  camera.query_object_input_port())
 
 diagram = builder.Build()
-context = diagram.CreateDefaultContext()
 
-meshcat.load()
-diagram.Publish(context)
+# Evaluate the camera output ports to get the images.
+context = diagram.CreateDefaultContext()
+camera_context = diagram.GetSubsystemContext(camera, context)
+color_image = camera.color_image_output_port().Eval(camera_context)
+depth_image = camera.depth_image_32F_output_port().Eval(camera_context)
+
+if use_meshcat:
+  meshcat.load()
+  diagram.Publish(context)
+
+# Plot the two images.
+plt.subplot(121)
+plt.imshow(color_image.data)
+plt.title('Color image')
+plt.subplot(122)
+plt.imshow(np.squeeze(depth_image.data))
+plt.title('Depth image')
+plt.show()
