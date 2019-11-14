5dd503bee8eedc82133e072ca1c436f6734c0514
Jamie Snape <jamie.snape@kitware.com>
2019-11-14 13:21:00 -0500

    Fix PEP8 issues

--- src/planar_iiwa/viz_test.py
+++ src/planar_iiwa/viz_test.py
@@ -5,12 +5,14 @@ from underactuated import PlanarSceneGraphVisualizer
 builder = drake.systems.framework.DiagramBuilder()
 plant, scene_graph = drake.multibody.plant.AddMultibodyPlantSceneGraph(builder)
 
-drake.multibody.parsing.Parser(plant, scene_graph).AddModelFromFile(drake.common.FindResourceOrThrow("drake/manipulation/models/iiwa_description/urdf/planar_iiwa14_spheres_dense_elbow_collision.urdf"))
+drake.multibody.parsing.Parser(plant, scene_graph).AddModelFromFile(
+    drake.common.FindResourceOrThrow(
+        "drake/manipulation/models/iiwa_description/urdf/planar_iiwa14_spheres_dense_elbow_collision.urdf"))  # noqa
 plant.Finalize()
 
 visualizer = builder.AddSystem(PlanarSceneGraphVisualizer(scene_graph))
 builder.Connect(scene_graph.get_pose_bundle_output_port(),
-        visualizer.get_input_port(0))
+                visualizer.get_input_port(0))
 
 diagram = builder.Build()
 
