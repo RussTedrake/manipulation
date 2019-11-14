aff6d200ca33057bf992e2bd8ee25bc67fa40bed
Jamie Snape <jamie.snape@kitware.com>
2019-11-14 13:20:53 -0500

    Fix prerequisite installation scripts for latest drake

--- setup/mac/install_prereqs.sh
+++ setup/mac/install_prereqs.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/zsh
 
-# Copyright (c) 2017, Massachusetts Institute of Technology.
+# Copyright (c) 2019, Massachusetts Institute of Technology.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -13,31 +13,31 @@
 #   this list of conditions and the following disclaimer in the documentation
 #   and/or other materials provided with the distribution.
 #
-# * Neither the name of the Massachusetts Institute of Technology nor the names
-#   of its contributors may be used to endorse or promote products derived from
-#   this software without specific prior written permission.
+# * Neither the name of the copyright holder nor the names of its contributors
+#   may be used to endorse or promote products derived from this software
+#   without specific prior written permission.
 #
-# THIS SOFTWARE IS PROVIDED BY THE MASSACHUSETTS INSTITUTE OF TECHNOLOGY AND
-# CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
-# NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
-# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE MASSACHUSETTS
-# INSTITUTE OF TECHNOLOGY OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
-# OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
-# EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
 
-set -euo pipefail
+set -euxo pipefail
 
 if [[ "${EUID}" -eq 0 ]]; then
   echo 'This script must NOT be run as root' >&2
   exit 1
 fi
 
-if ! command -v javac &>/dev/null; then
-  echo 'Java is NOT installed' >&2
+if [[ "$(uname -s)" != 'Darwin' || "$(uname -r)" < 17 ]]; then
+  echo 'ERROR: This script requires macOS Mojave (10.14) or Catalina (10.15)' >&2
   exit 2
 fi
 
@@ -45,18 +45,18 @@ if ! command -v brew &>/dev/null; then
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-# NOTE: Assumes that the drake (binary) install_prereqs script will also be run.  Only install what we need to download drake and any additional requirements for this package.
+# NOTE: Assumes that the drake (binary) install_prereqs script will also be
+# run. Only install what we need to download drake and any additional
+# requirements for this project.
 
-#brew update
-#brew upgrade
-
-# Install development and Drake dependencies.
-#brew install $(tr '\n' ' ' <<EOF
-#EOF
-#)
-
-pip3 install --upgrade $(tr '\n' ' ' <<EOF
-open3d
+brew install $(tr '\n' ' ' <<EOF
+numpy
+python
 EOF
 )
 
+pip3 install --disable-pip-version-check --upgrade $(tr '\n' ' ' <<EOF
+open3d
+pycodestyle
+EOF
+)
