938ac529721f86cb01576533b0bb40ab0e0f20af
Russ Tedrake <russt@mit.edu>
2020-01-12 05:38:41 -0500

    fix planar_iiwa viz_test

--- src/planar_iiwa/viz_test.py
+++ src/planar_iiwa/viz_test.py
@@ -7,7 +7,9 @@ from pydrake.multibody.plant import AddMultibodyPlantSceneGraph
 from pydrake.systems.analysis import Simulator
 from pydrake.systems.framework import DiagramBuilder
 from pydrake.systems.primitives import ConstantVectorSource
-from pydrake.systems.planar_scenegraph_visualizer import PlanarSceneGraphVisualizer
+from pydrake.systems.planar_scenegraph_visualizer import (
+    PlanarSceneGraphVisualizer
+)
 
 builder = DiagramBuilder()
 plant, scene_graph = AddMultibodyPlantSceneGraph(builder)
@@ -21,11 +23,12 @@ builder.Connect(scene_graph.get_pose_bundle_output_port(),
                 visualizer.get_input_port(0))
 
 torque_command = builder.AddSystem(ConstantVectorSource(np.zeros(3)))
-builder.Connect(torque_command.get_output_port(0), plant.get_actuation_input_port())
+builder.Connect(torque_command.get_output_port(0),
+                plant.get_actuation_input_port())
 
 diagram = builder.Build()
 
 simulator = Simulator(diagram)
 simulator.set_target_realtime_rate(1.0)
 
-simulator.AdvanceTo(5)
+simulator.AdvanceTo(1)
