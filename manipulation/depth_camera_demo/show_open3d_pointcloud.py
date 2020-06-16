d0ab14ec56dc952623577e0ea3017dfa980be210
Jamie Snape <jamie.snape@kitware.com>
2020-06-16 12:59:39 -0400

    Run buildifier and yapf

--- manipulation/depth_camera_demo/show_open3d_pointcloud.py
+++ manipulation/depth_camera_demo/show_open3d_pointcloud.py
@@ -13,7 +13,6 @@ indices = np.all(np.isfinite(point_cloud.xyzs()), axis=0)
 
 pcd = o3d.geometry.PointCloud()
 pcd.points = o3d.utility.Vector3dVector(point_cloud.xyzs()[:, indices].T)
-pcd.colors = o3d.utility.Vector3dVector(
-    point_cloud.rgbs()[:, indices].T / 255.)
+pcd.colors = o3d.utility.Vector3dVector(point_cloud.rgbs()[:, indices].T / 255.)
 
 o3d.visualization.draw_geometries([pcd])
--- manipulation/depth_camera_demo/station_cameras.py
+++ manipulation/depth_camera_demo/station_cameras.py
@@ -17,10 +17,10 @@ context = station.CreateDefaultContext()
 camera_names = station.get_camera_names()
 index = 1
 for name in camera_names:
-    color_image = station.GetOutputPort(
-        "camera_" + name + "_rgb_image").Eval(context)
-    depth_image = station.GetOutputPort(
-        "camera_" + name + "_depth_image").Eval(context)
+    color_image = station.GetOutputPort("camera_" + name +
+                                        "_rgb_image").Eval(context)
+    depth_image = station.GetOutputPort("camera_" + name +
+                                        "_depth_image").Eval(context)
 
     plt.subplot(len(camera_names), 2, index)
     plt.imshow(color_image.data)
