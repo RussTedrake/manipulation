21cc3122575b15ab9d2bdde01d8451167fe4afdf
Jamie Snape <jamie.snape@kitware.com>
2020-06-16 10:56:46 -0400

    Update setup scripts

--- scripts/setup/ubuntu/18.04/install_prereqs.sh
+++ scripts/setup/ubuntu/18.04/install_prereqs.sh
@@ -32,39 +32,64 @@
 set -euxo pipefail
 
 if [[ "${EUID}" -ne 0 ]]; then
-  echo 'This script must be run as root' >&2
+  echo 'ERROR: This script must be run as root' >&2
   exit 1
 fi
 
-apt-get update -qq
-apt-get install -o Dpkg::Use-Pty=0 -qy --no-install-recommends lsb-release
+if command -v conda &>/dev/null; then
+  echo 'WARNING: Anaconda is NOT supported. Please remove the Anaconda bin directory from the PATH.' >&2
+fi
+
+apt-get update -qq || (sleep 15; apt-get update -qq)
+
+apt-get install -o APT::Acquire::Retries=4 -o Dpkg::Use-Pty=0 -qy \
+  --no-install-recommends lsb-release
 
 if [[ "$(lsb_release -cs)" != 'bionic' ]]; then
   echo 'ERROR: This script requires Ubuntu 18.04 (Bionic)' >&2
   exit 2
 fi
 
-# NOTE: Assumes that the drake (binary) install_prereqs script will also be
-# run. Only install what we need to download drake and any additional
-# requirements for this project.
+apt-get install -o APT::Acquire::Retries=4 -o Dpkg::Use-Pty=0 -qy \
+  --no-install-recommends ca-certificates gnupg
 
-apt-get update -qq
+APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=1 apt-key adv \
+  -q --fetch-keys https://bazel.build/bazel-release.pub.gpg
 
-apt-get install -o Dpkg::Use-Pty=0 -qy --no-install-recommends $(tr '\n' ' ' <<EOF
-pycodestyle
+echo 'deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8' \
+  > /etc/apt/sources.list.d/bazel.list
+
+apt-get update -qq || (sleep 15; apt-get update -qq)
+
+apt-get install -o APT::Acquire::Retries=4 -o Dpkg::Use-Pty=0 -qy \
+  --no-install-recommends $(cat <<EOF
+bazel
+jupyter
+jupyter-nbconvert
+jupyter-notebook
+locales
 python3
 python3-ipywidgets
 python3-matplotlib
-python3-notebook
 python3-numpy
 python3-pip
 python3-setuptools
 python3-wheel
 python3-widgetsnbextension
+tidy
+wget
 EOF
 )
 
-pip3 install --disable-pip-version-check $(tr '\n' ' ' <<EOF
+locale-gen en_US.UTF-8
+
+LC_CTYPE=en_US.UTF-8 LANG=en_US.UTF-8 pip3 install --disable-pip-version-check $(cat <<EOF
 open3d
 EOF
 )
+
+jupyter nbextension enable --system --py widgetsnbextension
+
+if [[ -z "${LANG:-}" && -z "${LC_ALL:-}" ]]; then
+  echo 'WARNING: LANG and LC_ALL environment variables are NOT set. Please export LANG=en_US.UTF-8 LC_ALL=en_US.UTF-8.' >&2
+fi
