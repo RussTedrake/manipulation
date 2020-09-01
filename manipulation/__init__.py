a511f7b86dbc59db5276fc35caa5092297e50ed6
Russ Tedrake <russt@mit.edu>
2020-08-31 21:30:08 -0400

    Add setup_manipulation_colab.py

---
+++ manipulation/__init__.py
@@ -0,0 +1,1 @@
+# Empty file (for now) to define manipulation as a package.
---
+++ scripts/setup/setup_manipulation_colab.py
@@ -0,0 +1,37 @@
+import importlib
+import os
+import sys
+from urllib.request import urlretrieve
+
+
+def setup_drake(*, version, build):
+    urlretrieve(f"https://drake-packages.csail.mit.edu/drake/{build}/drake-{version}/setup_drake_colab.py",
+                "setup_drake_colab.py")
+    from setup_drake_colab import setup_drake
+    setup_drake(version=version, build=build)
+
+def setup_manipulation(*, manipulation_sha, drake_version, drake_build):
+    setup_drake(version=drake_version, build=drake_build)
+
+    path = "/opt/manipulation"
+
+    # Clone the repo (if necessary).
+    if not os.path.isdir(path):
+        subprocess.run(['git', 'clone', 
+            'https://github.com/RussTedrake/manipulation.git', path])
+
+    # Checkout the sha.
+    subprocess.run(['git', 'checkout', manipulation_sha], cwd=path);
+
+    # Set the path (if necessary).
+    spec = importlib.util.find_spec('manipulation')
+    if spec is None:
+        sys.path.append(path)
+        spec = importlib.util.find_spec('manipulation')
+
+    # Confirm that we now have manipulation on the path.
+    assert spec is not None, (
+        "Installation failed.  find_spec('manipulation') returned None.")
+    assert path in spec.origin, (
+        "Installation failed.  find_spec is locating manipulation, but not "
+        "in the expected path.")
