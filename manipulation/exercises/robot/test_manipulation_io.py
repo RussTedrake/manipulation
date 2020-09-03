b899fa50758ee7b7e685c520c02cc0800368fb6e
Terry Suh <hjsuh94@gmail.com>
2020-09-03 01:43:31 -0400

    set 0 tests and figures

--- manipulation/exercises/robot/test_manipulation_io.py
+++ manipulation/exercises/robot/test_manipulation_io.py
@@ -13,14 +13,15 @@ class TestManipulationIO(unittest.TestCase):
     @weight(5)
     @timeout_decorator.timeout(1.)
     def test_dynamics(self):
-        """Test state space dynamics"""
+        """Test get_velocity implementation"""
         f = self.notebook_locals['get_velocity']
         station = self.notebook_locals['station']
         station_context = self.notebook_locals['station_context']
-        
-    np.random.seed(7)
-    for i in range(10):
-        test_vel = np.random.rand(7)  # draw 7 random numbers
-        station.SetIiwaVelocity(station_context, test_vel)
-        eval_vel = f(station, station_context)
-        self.assertLessEqual(np.linalg.norm(test_vel - eval_vel), 1e-6, 'error_message')
+
+        np.random.seed(7)
+        for i in range(10):
+            test_vel = np.random.rand(7)  # draw 7 random numbers
+            station.SetIiwaVelocity(station_context, test_vel)
+            eval_vel = f(station, station_context)
+            self.assertLessEqual(np.linalg.norm(test_vel - eval_vel), 1e-6,
+                                 'get_velocity implementation is not correct!')
