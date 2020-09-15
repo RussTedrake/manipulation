31eb5178caa52620f293b9f8c56ac99026a5faa6
Russ Tedrake <russt@mit.edu>
2020-09-15 12:59:29 -0400

    workaround for missing setup script on continuous

--- scripts/setup/setup_manipulation_colab.py
+++ scripts/setup/setup_manipulation_colab.py
@@ -6,8 +6,12 @@ from urllib.request import urlretrieve
 
 
 def setup_drake(*, version, build):
-    urlretrieve(f"https://drake-packages.csail.mit.edu/drake/{build}/drake-{version}/setup_drake_colab.py",
-                "setup_drake_colab.py")
+    if build == "continuous":  # This is a hack!
+        urlretrieve(f"https://drake-packages.csail.mit.edu/drake/nightly/drake-20200915/setup_drake_colab.py",
+                    "setup_drake_colab.py")
+    else:
+        urlretrieve(f"https://drake-packages.csail.mit.edu/drake/{build}/drake-{version}/setup_drake_colab.py",
+                    "setup_drake_colab.py")
     from setup_drake_colab import setup_drake
     setup_drake(version=version, build=build)
 
