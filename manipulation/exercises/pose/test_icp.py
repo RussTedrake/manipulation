b0c98063da10d2270e63f660a4a89288903e80b2
Terry Suh <hjsuh94@gmail.com>
2020-09-23 02:24:59 -0400

    Tests for problem set 4

---
+++ manipulation/exercises/pose/test_icp.py
@@ -0,0 +1,94 @@
+import unittest
+import timeout_decorator
+from gradescope_utils.autograder_utils.decorators import weight
+import numpy as np
+from pydrake.all import RigidTransform, RotationMatrix
+from sklearn.neighbors import NearestNeighbors
+
+
+def least_squares_transform(scene, model):
+    X_BA = RigidTransform()
+    mu_m = np.mean(model, axis=0)
+    mu_s = np.mean(scene, axis=0)
+
+    W = (scene - mu_s).T.dot(model - mu_m)
+    U, Sigma, Vh = np.linalg.svd(W)
+    R_star = U.dot(Vh)
+
+    if np.linalg.det(R_star) < 0:
+        Vh[-1] *= -1
+        R_star = U.dot(Vh)
+
+    t_star = mu_s - R_star.dot(mu_m)
+
+    X_BA.set_rotation(RotationMatrix(R_star))
+    X_BA.set_translation(t_star)
+
+    return X_BA
+
+
+class TestICP(unittest.TestCase):
+
+    def __init__(self, test_name, notebook_locals):
+        super().__init__(test_name)
+        self.notebook_locals = notebook_locals
+        # random rotation
+        theta_z, theta_y, theta_x = np.random.rand(3) * np.pi / 4
+        rot_z = RotationMatrix.MakeZRotation(theta_z)
+        rot_y = RotationMatrix.MakeZRotation(theta_y)
+        rot_x = RotationMatrix.MakeZRotation(theta_x)
+        R_BA = rot_z.multiply(rot_y).multiply(rot_x)
+        # random translation
+        dx, dy, dz = np.random.rand(3) * 1.0
+        p_BA = [dx, dy, dz]
+
+        X_BA = RigidTransform(R_BA, p_BA)
+
+        self.model = self.notebook_locals["pointcloud_model"]
+        self.scene = X_BA.multiply(self.model.T).T
+
+    @weight(3)
+    @timeout_decorator.timeout(1.)
+    def test_least_square(self):
+        """Test least square transform"""
+        f = self.notebook_locals["least_squares_transform"]
+        X_BA_test = f(self.scene, self.model)
+        X_BA_true = least_squares_transform(self.scene, self.model)
+
+        # check answer
+        result = X_BA_true.inverse().multiply(X_BA_test)
+
+        self.assertTrue(np.allclose(result.GetAsMatrix4(), np.eye(4)),
+                        'least square transform is incorrect')
+
+    @weight(3)
+    @timeout_decorator.timeout(1.)
+    def test_icp(self):
+        """Test icp implementation"""
+        f = self.notebook_locals["icp"]
+        nearest_neighbors = self.notebook_locals["nearest_neighbors"]
+
+        X_BA_test, mean_error_test, num_iters_test = f(self.scene, self.model)
+
+        num_iters = 0
+        mean_error = 0.0
+        max_iterations = 20
+        tolerance = 1e-3
+        prev_error = 0
+        X_BA = RigidTransform()
+
+        while True:
+            num_iters += 1
+            distances, indices = nearest_neighbors(
+                self.scene,
+                X_BA.multiply(self.model.T).T)
+            X_BA = least_squares_transform(self.scene, self.model[indices])
+            mean_error = np.mean(distances)
+            if abs(mean_error -
+                   prev_error) < tolerance or num_iters >= max_iterations:
+                break
+            prev_error = mean_error
+
+        result = X_BA.multiply(X_BA_test.inverse())
+        self.assertTrue(np.allclose(result.GetAsMatrix4(), np.eye(4)),
+                        'icp implementation is incorrect')
---
+++ manipulation/exercises/pose/test_pose_estimation.py
@@ -0,0 +1,79 @@
+import unittest
+import timeout_decorator
+from gradescope_utils.autograder_utils.decorators import weight
+import numpy as np
+import os
+
+from manipulation import FindResource
+
+
+class TestPoseEstimation(unittest.TestCase):
+
+    def __init__(self, test_name, notebook_locals):
+        super().__init__(test_name)
+        self.notebook_locals = notebook_locals
+
+    @weight(4)
+    @timeout_decorator.timeout(5.)
+    def test_filtered_pointcloud(self):
+        """Test filtered pointcloud"""
+        f = self.notebook_locals["scene_pcl_np_filtered"].tolist()
+        f_target = np.load(
+            FindResource("exercises/pose/pcl_filtered.npy")).tolist()
+
+        f = set(tuple(row) for row in f)
+        f_target = set(tuple(row) for row in f_target)
+
+        inliers = len(set(f).intersection(f_target))
+        outliers = len(f) - inliers
+
+        self.assertLessEqual(
+            outliers, 80,
+            'You have too many false positives (too many outliers)')
+        self.assertLessEqual(
+            len(f_target) - inliers, 80,
+            'You have too many false negatives (not enough inliers)')
+
+    @weight(1)
+    @timeout_decorator.timeout(1.)
+    def test_xyz_loose(self):
+        """Test XYZ with loose bounds"""
+        f = self.notebook_locals["xyz"]
+        target = 0.02
+
+        for i in range(3):
+            self.assertLessEqual(abs(f[i]), target,
+                                 "XYZ"[i] + " translation is too off")
+
+    @weight(1)
+    @timeout_decorator.timeout(1.)
+    def test_xyz_tight(self):
+        """Test XYZ with tight bounds"""
+        f = self.notebook_locals["xyz"]
+        target = 0.005
+
+        for i in range(3):
+            self.assertLessEqual(abs(f[i]), target,
+                                 "XYZ"[i] + " translation is too off")
+
+    @weight(1)
+    @timeout_decorator.timeout(1.)
+    def test_rpy_loose(self):
+        """Test RPY with loose bounds"""
+        f = self.notebook_locals["rpy"]
+        target = 0.5
+
+        for i in range(3):
+            self.assertLessEqual(abs(f[i]), target,
+                                 "RPY"[i] + " translation is too off")
+
+    @weight(1)
+    @timeout_decorator.timeout(1.)
+    def test_rpy_tight(self):
+        """Test RPY with tight bounds"""
+        f = self.notebook_locals["rpy"]
+        target = 0.05
+
+        for i in range(3):
+            self.assertLessEqual(abs(f[i]), target,
+                                 "RPY"[i] + " translation is too off")
