00cefbe5b7dd8e9eb2d78c5e607a656d7d65150b
Eric Cousineau <eric.cousineau@tri.global>
2020-10-19 09:24:30 -0400

    trim deps for drake dev, use np.dot for symbolic, make no-teleop sim brief

--- scripts/setup/ubuntu/18.04/install_prereqs.sh
+++ scripts/setup/ubuntu/18.04/install_prereqs.sh
@@ -31,6 +31,14 @@
 
 set -euxo pipefail
 
+as-user() {
+  if [[ -n "${SUDO_USER:+D}" ]]; then
+    sudo -u ${SUDO_USER} "$@"
+  else
+    "$@"
+  fi
+}
+
 if [[ "${EUID}" -ne 0 ]]; then
   echo 'ERROR: This script must be run as root' >&2
   exit 1
@@ -53,17 +61,10 @@ fi
 apt-get install -o APT::Acquire::Retries=4 -o Dpkg::Use-Pty=0 -qy \
   --no-install-recommends ca-certificates gnupg
 
-APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=1 apt-key adv \
-  -q --fetch-keys https://bazel.build/bazel-release.pub.gpg
-
-echo 'deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8' \
-  > /etc/apt/sources.list.d/bazel.list
-
 apt-get update -qq || (sleep 15; apt-get update -qq)
 
 apt-get install -o APT::Acquire::Retries=4 -o Dpkg::Use-Pty=0 -qy \
   --no-install-recommends $(cat <<EOF
-bazel
 jupyter
 jupyter-nbconvert
 jupyter-notebook
@@ -93,7 +94,7 @@ EOF
 
 locale-gen en_US.UTF-8
 
-jupyter nbextension enable --system --py widgetsnbextension
+as-user jupyter nbextension enable --py widgetsnbextension
 
 if [[ -z "${LANG:-}" && -z "${LC_ALL:-}" ]]; then
   echo 'WARNING: LANG and LC_ALL environment variables are NOT set. Please export LANG=en_US.UTF-8 LC_ALL=en_US.UTF-8.' >&2
