3c63eafe24853bd0061fb92a218e5170ea5ab2cd
Russ Tedrake <russt@mit.edu>
2021-09-21 11:10:14 -0400

    make a direct meshcat triad option, and use it in robot_painter

--- manipulation/meshcat_cpp_utils.py
+++ manipulation/meshcat_cpp_utils.py
@@ -6,12 +6,12 @@ from IPython.display import display, HTML, Javascript
 import numpy as np
 
 from pydrake.common import set_log_level
-from pydrake.geometry import Meshcat
+from pydrake.geometry import Meshcat, Cylinder, Rgba
 
 # imports for the pose sliders
 from collections import namedtuple
 from pydrake.common.value import AbstractValue
-from pydrake.math import RollPitchYaw, RigidTransform
+from pydrake.math import RollPitchYaw, RigidTransform, RotationMatrix
 from pydrake.systems.framework import LeafSystem, PublishEvent
 
 # imports for the joint sliders
@@ -317,3 +317,31 @@ class MeshcatJointSlidersThatPublish():
             time.sleep(.1)
 
         self._meshcat.DeleteButton("Stop JointSliders")
+
+
+def AddMeshcatTriad(meshcat,
+                    path,
+                    length=.25,
+                    radius=0.01,
+                    opacity=1.,
+                    X_PT=RigidTransform()):
+    meshcat.SetTransform(path, X_PT)
+    # x-axis
+    X_TG = RigidTransform(RotationMatrix.MakeYRotation(np.pi / 2),
+                          [length / 2., 0, 0])
+    meshcat.SetTransform(path + "/x-axis", X_TG)
+    meshcat.SetObject(path + "/x-axis", Cylinder(radius, length),
+                      Rgba(1, 0, 0, opacity))
+
+    # y-axis
+    X_TG = RigidTransform(RotationMatrix.MakeXRotation(np.pi / 2),
+                          [0, length / 2., 0])
+    meshcat.SetTransform(path + "/y-axis", X_TG)
+    meshcat.SetObject(path + "/y-axis", Cylinder(radius, length),
+                      Rgba(0, 1, 0, opacity))
+
+    # z-axis
+    X_TG = RigidTransform([0, 0, length / 2.])
+    meshcat.SetTransform(path + "/z-axis", X_TG)
+    meshcat.SetObject(path + "/z-axis", Cylinder(radius, length),
+                      Rgba(0, 0, 1, opacity))
