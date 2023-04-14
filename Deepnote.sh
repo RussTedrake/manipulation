915e9113443809a9c3400b57e31cc90e40b6b7ce
Russ Tedrake <russt@mit.edu>
2023-04-14 08:42:42 -0400

    add deepnote link

--- Deepnote.sh
+++ Deepnote.sh
@@ -7,3 +7,4 @@ docker build -f setup/docker/Dockerfile -t russtedrake/manipulation:$(git rev-pa
 docker push russtedrake/manipulation:$(git rev-parse --short HEAD)
 python3 htmlbook/publish_to_deepnote.py $(git rev-parse --short HEAD)
 echo "Remember to log on to deepnote and build the dockerfile in any one of the notebooks"
+echo "https://deepnote.com/workspace/$(cat Deepnote_workspace.txt)"
