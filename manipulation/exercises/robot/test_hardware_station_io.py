77ca5777c7584de248568a0a9c2eb910534d8c4a
Russ Tedrake <russt@mit.edu>
2023-09-06 22:28:41 -0400

    manipulation station i/o => hardware station i/o

--- manipulation/exercises/robot/test_hardware_station_io.py
+++ manipulation/exercises/robot/test_hardware_station_io.py
@@ -4,7 +4,7 @@ from gradescope_utils.autograder_utils.decorators import weight
 import numpy as np
 
 
-class TestManipulationIO(unittest.TestCase):
+class TestHardwareStationIO(unittest.TestCase):
     def __init__(self, test_name, notebook_locals):
         super().__init__(test_name)
         self.notebook_locals = notebook_locals
