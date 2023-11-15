8b6c55bba51f3d68df786b336c0a16e87ffd775c
Russ Tedrake <russt@mit.edu>
2023-11-15 08:30:20 -0500

    bump minimum python version to 3.10 to use newer scipy (and quiet the dependabot spam)

--- PoetryExport.sh
+++ PoetryExport.sh
@@ -7,11 +7,11 @@ poetry export --without-hashes --with dev > requirements.txt
 sed 's/matplotlib==3.7.3 ; python_version >= "3.8"/matplotlib==3.5.1 ; sys_platform == "linux"\nmatplotlib==3.7.3 ; sys_platform == "darwin"/' requirements.txt > requirements.txt.tmp && mv requirements.txt.tmp requirements.txt
 # Force torch to be cpu-only for bazel.
 awk '
-/torch==[0-9]+\.[0-9]+\.[0-9]+ ; python_version >= "3.8"/ {
+/torch==[0-9]+\.[0-9]+\.[0-9]+ ; python_version >= "3.10"/ {
     version=$1; sub(/^torch==/, "", version); sub(/ ;.*/, "", version);
     print "--find-links https://download.pytorch.org/whl/torch_stable.html";
-    print "torch==" version "+cpu ; python_version >= \"3.8\" and sys_platform == \"linux\"";
-    print "torch==" version " ; python_version >= \"3.8\" and sys_platform == \"darwin\"";
+    print "torch==" version "+cpu ; python_version >= \"3.10\" and sys_platform == \"linux\"";
+    print "torch==" version " ; python_version >= \"3.10\" and sys_platform == \"darwin\"";
     next;
 }
 1' requirements.txt > requirements-bazel.txt
