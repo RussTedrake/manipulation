50fcf24e71f736c68d0630abba2e5f00f3cff6da
Russ Tedrake <russt@mit.edu>
2023-12-24 16:14:40 -0500

    fix publishing after book refactor

--- book/Deepnote.sh
+++ book/Deepnote.sh
@@ -5,6 +5,6 @@ docker build -f setup/docker/Dockerfile -t russtedrake/manipulation:latest .
 docker push russtedrake/manipulation:latest
 docker build -f setup/docker/Dockerfile -t russtedrake/manipulation:$(git rev-parse --short HEAD) .
 docker push russtedrake/manipulation:$(git rev-parse --short HEAD)
-python3 htmlbook/publish_to_deepnote.py $(git rev-parse --short HEAD)
+python3 book/htmlbook/publish_to_deepnote.py $(git rev-parse --short HEAD)
 echo "Remember to log on to deepnote and build the dockerfile in any one of the notebooks"
-echo "https://deepnote.com/workspace/$(cat Deepnote_workspace.txt)"
+echo "https://deepnote.com/workspace/$(cat book/Deepnote_workspace.txt)"
