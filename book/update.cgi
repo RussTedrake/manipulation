7c3c753bc360f8371f86a52e78c6fd4be78a4d2d
Russ Tedrake <russt@mit.edu>
2023-12-30 17:24:25 -0500

    build the documentation on the server rather than keeping the generated content in the source tree

--- book/update.cgi
+++ book/update.cgi
@@ -11,4 +11,9 @@ print "pulling repo...<br/>";
 system 'git fetch origin && git reset --hard origin/master';
 system 'git submodule update --init --recursive';
 print "<br/>done.";
+
+print "building documentation...<br/>";
+system 'rm -rf book/python && sphinx-build -M html manipulation /tmp/manip_doc && cp -r /tmp/manip_doc/html book/python';
+print "<br/>done.";
+
 print $r->end_html;
--- manipulation/conf.py
+++ manipulation/conf.py
@@ -2,23 +2,18 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os
+import sys
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+sys.path.insert(0, os.path.abspath(".."))
 
 project = "manipulation"
 copyright = "2023, Russ Tedrake"
 author = "Russ Tedrake"
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
 templates_path = ["_templates"]
 exclude_patterns = ["**/*.md"]
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
     "myst_parser",  # To enable autodoc from markdown
     "sphinx.ext.autodoc",
@@ -27,11 +22,7 @@ extensions = [
     "sphinx.ext.intersphinx",
 ]
 
-# Option available in Sphinx 1.5+.
 napoleon_include_init_with_doc = True
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
