634aced54b4504d3c8f380d726d34f8826722238
Siddharth Ancha <sancha@mit.edu>
2024-07-08 17:31:32 -0400

    Implement recursive functions in `DirectivesTree`

--- manipulation/directives_tree.py
+++ manipulation/directives_tree.py
@@ -1,7 +1,7 @@
 import dataclasses as dc
 from typing import Dict, List, Literal, Set, Tuple
 
-from pydrake.all import AddFrame, AddWeld, ModelDirective, ScopedName
+from pydrake.all import AddFrame, AddModel, AddWeld, ModelDirective, ScopedName
 
 """
 ===============================================================================
@@ -37,8 +37,11 @@ class Edge:
 
 class DirectivesTree:
     def __init__(self, flattened_directives: List[ModelDirective]):
+        self.add_model_directives: Dict[str, AddModel] = dict()
+
         # Names of frames added by `add_frame` directives
-        self.frame_names: Set[str] = {}
+        # The default "world" frame exists implicitly.
+        self.frame_names: Set[str] = {"world"}
 
         # Names of models added by `add_model` directives
         self.model_names: Set[str] = {}
@@ -50,14 +53,15 @@ class DirectivesTree:
         for d in flattened_directives:
             if d.add_model:
                 self.model_names.add(d.add_model.name)
+                self.add_model_directives[d.add_model.name] = d.add_model
             if d.add_frame:
                 self.frame_names.add(d.add_frame.name)
 
         # Create edges.
         for d in flattened_directives:
             if d.add_weld:
-                parent = self.make_node(d.add_weld.parent)
-                child = self.make_node(d.add_weld.child)
+                parent = self._MakeNode(d.add_weld.parent)
+                child = self._MakeNode(d.add_weld.child)
                 edge = Edge(parent, child, d.add_weld)
                 if parent not in self.edges:
                     self.edges[parent] = {edge}
@@ -65,15 +69,15 @@ class DirectivesTree:
                     self.edges[parent].add(edge)
 
             if d.add_frame:
-                parent = self.make_node(d.add_frame.X_PF.base_frame)
-                child = self.make_node(d.add_frame.name)
+                parent = self._MakeNode(d.add_frame.X_PF.base_frame)
+                child = self._MakeNode(d.add_frame.name)
                 edge = Edge(parent, child, d.add_weld)
                 if parent not in self.edges:
                     self.edges[parent] = {edge}
                 else:
                     self.edges[parent].add(edge)
 
-    def make_node(self, name: str):
+    def _MakeNode(self, name: str):
         # Check if this is an added frame
         if name in self.frame_names:
             return Node(name, "frame")
@@ -90,10 +94,84 @@ class DirectivesTree:
 
     def GetWeldedChildrenAndDirectives(
         self, model_instance_names: List[str]
-    ) -> Tuple[List[str], List[ModelDirective]]:
-        pass
+    ) -> Tuple[Set[str], Set[ModelDirective]]:
+
+        def _RecursionCall(node: Node) -> Tuple[Set[str], Set[ModelDirective]]:
+            """
+            Args:
+                node (Node): The node to start this recursion call from.
+
+            Returns:
+                Set[str]: Names of descendant models that are welded to the
+                    input node (including itself).
+                Set[ModelDirective]: The directives that need to be added to
+                    weld the descendant models to the input node.
+            """
+            descendants: Set[str] = {}
+            directives: Set[ModelDirective] = {}
+
+            # if the node is a model instance, add its AddModel directive,
+            # and add the model's name to the set of descendants.
+            if node.type == "model":
+                descendants.add(node.name)
+                directives.add(self.add_model_directives[node.name])
+
+            for edge in self.edges.get(node, set()):
+                # add the descendant and directives of the child node
+                _descendants, _directives = _RecursionCall(edge.child)
+                descendants.update(_descendants)
+                directives.update(_directives)
+
+                # if the child node has non-zero descendants, add the
+                # edge directive that leads to the child node.
+                if len(_descendants) > 0:
+                    directives.add(edge.directive)
+
+            return descendants, directives
+
+        descendants: Set[str] = {}
+        directives: Set[ModelDirective] = {}
+        for model_instance_name in model_instance_names:
+            assert model_instance_name in self.add_model_directives
+            node = Node(model_instance_name, "model")
+            _descendants, _directives = _RecursionCall(node)
+            descendants.update(_descendants)
+            directives.update(_directives)
+
+        return descendants, directives
 
     def GetWeldToWorldDirectives(
         self, model_instance_names: List[str]
-    ) -> List[ModelDirective]:
-        pass
+    ) -> Set[ModelDirective]:
+
+        def _RecursionCall(node: Node) -> Set[ModelDirective]:
+            """
+            Args:
+                node (Node): The node to start this recursion call from.
+
+            Returns:
+                Set[ModelDirective]: The directives that need to be added to
+                    weld all `model_instance_names` to the "world" frame.
+            """
+            directives: Set[ModelDirective] = {}
+
+            # if the node is one of the model instances, add its AddModel
+            # directive and return.
+            if node.type == "model" and node.name in model_instance_names:
+                directives.add(self.add_model_directives[node.name])
+                return directives
+
+            for edge in self.edges.get(node, set()):
+                # add the directives of the child node
+                _directives = _RecursionCall(edge.child)
+                directives.update(_directives)
+
+                # if the child node has non-zero directives, add the
+                # edge directive that leads to the child node.
+                if len(_directives) > 0:
+                    directives.add(edge.directive)
+
+            return directives
+
+        world_node = Node("world", "frame")
+        return _RecursionCall(world_node)
