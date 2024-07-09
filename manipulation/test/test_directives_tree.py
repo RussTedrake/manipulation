2f87a9d4f0e047125981e1430cd4ed57fd44f236
Siddharth Ancha <sancha@mit.edu>
2024-07-08 23:52:51 -0400

    Convert to explicit directives

--- manipulation/test/test_directives_tree.py
+++ manipulation/test/test_directives_tree.py
@@ -1,69 +1,104 @@
+import typing
 import unittest
 
-from pydrake.all import StartMeshcat
+from pydrake.all import (
+    AddFrame,
+    AddModel,
+    AddWeld,
+    IiwaDriver,
+    ModelDirective,
+    Rotation,
+    StartMeshcat,
+    Transform,
+)
 
-from manipulation.station import LoadScenario, MakeHardwareStation
+from manipulation.station import MakeHardwareStation, Scenario
 
 
 class DirectivesTreeTest(unittest.TestCase):
+    def GetFlattenedDirectives(self) -> typing.List[ModelDirective]:
+        return [
+            ModelDirective(
+                add_model=AddModel(
+                    name="iiwa",
+                    file="package://drake_models/iiwa_description/urdf/iiwa14_no_collision.urdf",
+                )
+            ),
+            ModelDirective(
+                add_frame=AddFrame(
+                    name="iiwa_origin",
+                    X_PF=Transform(
+                        base_frame="world",
+                        translation=[0, 0.765, 0],
+                    ),
+                )
+            ),
+            ModelDirective(
+                add_weld=AddWeld(
+                    parent="iiwa_origin",
+                    child="iiwa::base",
+                )
+            ),
+            ModelDirective(
+                add_model=AddModel(
+                    name="wsg",
+                    file="package://drake_models/wsg_50_description/sdf/schunk_wsg_50_with_tip.sdf",
+                )
+            ),
+            ModelDirective(
+                add_frame=AddFrame(
+                    name="iiwa::wsg_attach",
+                    X_PF=Transform(
+                        base_frame="iiwa::iiwa_link_7",
+                        translation=[0, 0, 0.114],
+                        rotation=Rotation.Rpy(deg=[90.0, 0.0, 68.0]),
+                    ),
+                )
+            ),
+            ModelDirective(
+                add_weld=AddWeld(
+                    parent="iiwa::wsg_attach",
+                    child="wsg::body",
+                )
+            ),
+            ModelDirective(
+                add_model=AddModel(
+                    name="table",
+                    file="package://drake_models/manipulation_station/table_wide.sdf",
+                )
+            ),
+            ModelDirective(
+                add_frame=AddFrame(
+                    name="table_origin",
+                    X_PF=Transform(
+                        base_frame="world",
+                        translation=[0.4, 0.3825, 0.0],
+                        rotation=Rotation.Rpy(deg=[0.0, 0.0, 0.0]),
+                    ),
+                )
+            ),
+            ModelDirective(
+                add_weld=AddWeld(
+                    parent="table_origin",
+                    child="table::table_body",
+                )
+            ),
+        ]
+
+    def LoadScenario(self):
+        scenario = Scenario()
+        scenario.directives = self.GetFlattenedDirectives()
+        scenario.model_drivers = {
+            "iiwa": IiwaDriver(
+                control_mode="position_only",
+                hand_model_name="wsg",
+            )
+        }
+        return scenario
+
     def test_load_scenario(self):
-        scenario_data = """
-directives:
-    # Add IIWA
-    - add_model:
-        name: iiwa
-        file: package://drake_models/iiwa_description/urdf/iiwa14_no_collision.urdf
-
-    - add_frame:
-        name: iiwa_origin
-        X_PF:
-            base_frame: world
-            translation: [0, 0.765, 0]
-    
-    - add_weld:
-        parent: iiwa_origin
-        child: iiwa::base
-
-    # Add schunk
-    - add_model:
-        name: wsg
-        file: package://drake_models/wsg_50_description/sdf/schunk_wsg_50_with_tip.sdf
-
-    - add_frame:
-        name: iiwa::wsg_attach
-        X_PF:
-            base_frame: iiwa::iiwa_link_7
-            translation: [0, 0, 0.114]
-            rotation: !Rpy { deg: [90.0, 0.0, 68.0 ]}
-
-    - add_weld:
-        parent: iiwa::wsg_attach
-        child: wsg::body
-
-    # Add object
-    - add_model:
-        name: table
-        file: package://drake_models/manipulation_station/table_wide.sdf
-
-    - add_frame:
-        name: table_origin
-        X_PF:
-            base_frame: world
-            translation: [0.4, 0.3825, 0.0]
-            rotation: !Rpy { deg: [0., 0., 0.]}
-
-    - add_weld:
-        parent: table_origin
-        child: table::table_body
-    
-model_drivers:
-    iiwa: !IiwaDriver
-        control_mode: position_only
-        hand_model_name: wsg
-"""
-
         meshcat = StartMeshcat()
-        scenario = LoadScenario(data=scenario_data)
+        scenario = self.LoadScenario()
         station = MakeHardwareStation(scenario, meshcat, hardware=False)
 
 
