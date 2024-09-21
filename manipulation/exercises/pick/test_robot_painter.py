e9c18e09c790e7364aabc2be46f8cfbee1da0233
Shruti Garg <53955086+shrutigarg914@users.noreply.github.com>
2024-09-21 14:37:51 -0400

    fixing grader for robot painter (#345)
    * fixing grader for robot painter
    
    * more tolerance in checking
    
    * add check for angle
    
    * fix typo
    
    * switch keyframe visual
    
    * adding check for number of key frames

--- manipulation/exercises/pick/test_robot_painter.py
+++ manipulation/exercises/pick/test_robot_painter.py
@@ -18,8 +18,14 @@ class TestRobotPainter(unittest.TestCase):
         X_WC = self.notebook_locals["X_WCenter"]
         radius = self.notebook_locals["radius"]
         thetas = self.notebook_locals["thetas"]
+        num_key_frames = self.notebook_locals["num_key_frames"]
         # carry out computation
         output_frames = f(thetas, X_WC, radius)
+
+        # check number of key frames
+        self.assertEqual(
+            len(output_frames), num_key_frames, "wrong number of key frames"
+        )
 
         # test all key positions match radius
         for i, frame_i in enumerate(output_frames):
@@ -37,3 +43,21 @@ class TestRobotPainter(unittest.TestCase):
             test_center = z_cur * radius + pos_cur
             center_err = np.linalg.norm(test_center - X_WC.translation())
             self.assertLessEqual(center_err, 1e-6, "key frame orientations incorrect!")
+
+            # check if we're rotating counterclockwise
+            if i > 0:
+                X_G1W = output_frames[i - 1].inverse()
+                X_WG2 = frame_i
+                X_G1G2 = X_G1W @ X_WG2
+                rotation_angle = X_G1G2.rotation().ToAngleAxis().angle()
+                self.assertAlmostEqual(
+                    thetas[i] - thetas[i - 1],
+                    rotation_angle,
+                    2,
+                    "rotation angle incorrect",
+                )
+
+                rotation_direction = X_G1G2.rotation().ToAngleAxis().axis()[1]
+                self.assertAlmostEqual(
+                    rotation_direction, -1, 2, "rotating in the wrong direction"
+                )
