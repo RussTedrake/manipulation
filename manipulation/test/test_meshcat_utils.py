51820c05db9376dae910b467e0d6d0abeff659e6
Russ Tedrake <russt@mit.edu>
2025-01-04 16:02:39 -0500

    Port remaining tests in manipulation/tests to unittest
    Resolves #408

--- manipulation/test/test_meshcat_utils.py
+++ manipulation/test/test_meshcat_utils.py
@@ -1,14 +1,24 @@
+import unittest
+
 import numpy as np
 from pydrake.all import MathematicalProgram, Meshcat, Solve
 
 import manipulation.meshcat_utils as dut
 
-prog = MathematicalProgram()
-x = prog.NewContinuousVariables(2)
-prog.AddCost(x.dot(x))
-prog.AddBoundingBoxConstraint(-2, 2, x)
-result = Solve(prog)
 
-meshcat = Meshcat()
-X, Y = np.meshgrid(np.linspace(-3, 3, 35), np.linspace(-3, 3, 31))
-dut.plot_mathematical_program(meshcat, "test", prog, X, Y, result)
+class TestMeshcatUtils(unittest.TestCase):
+    def test_plot_mathematical_program(self):
+        prog = MathematicalProgram()
+        x = prog.NewContinuousVariables(2)
+        prog.AddCost(x.dot(x))
+        prog.AddBoundingBoxConstraint(-2, 2, x)
+        result = Solve(prog)
+
+        meshcat = Meshcat()
+        X, Y = np.meshgrid(np.linspace(-3, 3, 35), np.linspace(-3, 3, 31))
+        # Test that plotting doesn't raise any exceptions
+        dut.plot_mathematical_program(meshcat, "test", prog, X, Y, result)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- manipulation/test/test_utils.py
+++ manipulation/test/test_utils.py
@@ -1,11 +1,20 @@
+import unittest
 from datetime import date
 
 from manipulation.mustard_depth_camera_example import MustardExampleSystem
 from manipulation.utils import DrakeVersionGreaterThan, SystemHtml
 
-# Test DrakeVersionGreaterThan
-DrakeVersionGreaterThan(date(2023, 12, 1))
 
-# Test SystemHtml
-system = MustardExampleSystem()
-SystemHtml(system)
+class TestUtils(unittest.TestCase):
+    def test_drake_version(self):
+        # Test that this doesn't raise an exception
+        DrakeVersionGreaterThan(date(2023, 12, 1))
+
+    def test_system_html(self):
+        system = MustardExampleSystem()
+        # Test that this doesn't raise an exception
+        SystemHtml(system)
+
+
+if __name__ == "__main__":
+    unittest.main()
