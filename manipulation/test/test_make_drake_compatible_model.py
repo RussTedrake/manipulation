169a7e0e7f3501fa0aae389e9d48864cd2f393f2
Nicholas Pfaff <53228351+nepfaff@users.noreply.github.com>
2025-07-14 10:15:35 -0400

    Support SDF files in make_drake_compatible_model.py (#453)
    * Support SDF files in make_drake_compatible_model.py
    
    * Add test for make_drake_compatible_models SDF conversion

--- manipulation/test/test_make_drake_compatible_model.py
+++ manipulation/test/test_make_drake_compatible_model.py
@@ -68,8 +68,39 @@ class TestMakeDrakeCompatibleModel(unittest.TestCase):
         os.remove(output_filename)
 
     def test_sdf(self):
-        # TODO(russt): Implement this.
-        pass
+        original_filename = FindResource("test/models/test.sdf")
+        input_filename = original_filename.replace(".sdf", "_modified.sdf")
+        with open(original_filename, "r") as file:
+            original_content = file.read()
+        modified_content = original_content.replace(
+            "replace_me_in_test_with_absolute_path", os.path.dirname(input_filename)
+        )
+        with open(input_filename, "w") as file:
+            file.write(modified_content)
+        output_filename = tempfile.mktemp(suffix=".sdf")
+        package_map = PackageMap()
+        package_map.AddPackageXml(filename=FindResource("test/models/package.xml"))
+        MakeDrakeCompatibleModel(
+            input_filename=input_filename,
+            output_filename=output_filename,
+            package_map=package_map,
+        )
+        self.assertTrue(os.path.exists(output_filename))
+        with open(output_filename, "r") as f:
+            output_content = f.read()
+        self.assertIn("<uri>cube_from_stl.obj</uri>", output_content)
+        self.assertIn("<uri>cube_from_dae.obj</uri>", output_content)
+        self.assertIn("<uri>cube.obj</uri>", output_content)
+        self.assertIn(
+            f"<uri>file://{os.path.dirname(input_filename)}/cube_from_obj_scaled_1_2_3.obj</uri>",
+            output_content,
+        )
+        self.assertIn(
+            "<uri>package://manipulation_test_models/cube_from_obj_scaled_n1_1_1.obj</uri>",
+            output_content,
+        )
+        # Clean up the temp file
+        os.remove(output_filename)
 
     def test_mjcf(self):
         input_filename = FindResource("test/models/test.xml")
