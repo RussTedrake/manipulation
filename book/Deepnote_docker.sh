225b47c41147bed2c4dfc32afc439dd63fe86589
Russ Tedrake <russt@mit.edu>
2025-08-29 23:02:16 -0400

    re-enable docker CI (#470)

--- book/Deepnote_docker.sh
+++ book/Deepnote_docker.sh
@@ -3,11 +3,9 @@
 set -e
 
 docker pull robotlocomotion/drake:noble
-docker build -f setup/docker/Dockerfile -t russtedrake/manipulation:latest .
+docker buildx build --platform linux/amd64,linux/arm64 -f setup/docker/Dockerfile -t russtedrake/manipulation:latest --push .
 # Note: This could require `docker login`
-docker push russtedrake/manipulation:latest
-docker build -f setup/docker/Dockerfile -t russtedrake/manipulation:$(git rev-parse --short HEAD) .
-docker push russtedrake/manipulation:$(git rev-parse --short HEAD)
+docker buildx build --platform linux/amd64,linux/arm64 -f setup/docker/Dockerfile -t russtedrake/manipulation:$(git rev-parse --short HEAD) --push .
 git rev-parse --short HEAD > book/Deepnote_docker_sha.txt
 echo "Remember to run Deepnote.sh to actually push to Deepnote"
 echo "Remember to log on to deepnote and build the dockerfile in any one of the notebooks"
